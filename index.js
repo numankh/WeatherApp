4d115f90ad3a845179287a5bfeb8308795b4445e
numankh <khannuman15@gmail.com>
2019-11-24 09:45:28 -0500

    included yargs for interactivity

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const request = require('request');
+const argv = require('yargs').argv;
 
 let config = require('./config.js');
 let apiKey = config.keys.WEATHER_API_KEY;
 
-let city = 'centreville';
+// user can enter city from command line: node index.js -c Boston
+let city = argv.c || 'centreville';
 let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
 
 request(url, function (err, response, body) {
